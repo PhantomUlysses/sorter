0f01d138fe78fffba7ef6bb57957bfd243b5f749
Nikita Stasiulevich <nikitastas1694@gmail.com>
2018-09-19 23:16:35 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,30 +1,46 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.array = [];
+    this.compare = (left, right) => left - right;
   }
 
   add(element) {
     // your implementation
+    this.array.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.array[index];
   }
 
   get length() {
     // your implementation
+    return this.array.length;
   }
 
   toArray() {
     // your implementation
+    return this.array;
   }
 
   sort(indices) {
     // your implementation
+    var swap = [];
+    for (var i = 0; i < indices.length; i++) {
+      swap.push(this.array[indices[i]]);
+    }
+    swap.sort(this.compare);
+    indices.sort();
+    for (var i = 0; i < indices.length; i++) {
+      this.array[indices[i]] = swap[i];
+    }
   }
 
   setComparator(compareFunction) {
     // your implementation
+    this.compare =  compareFunction;
   }
 }
 
